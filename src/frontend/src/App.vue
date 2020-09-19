b8b4ecb6909796d77f9f73fe2eee775a2178072a
luca <luca9716@gmail.com>
2020-09-19 12:14:50 -0400

    basic functionality
    added team-box component
    some styling

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,31 +1,59 @@
 <template>
 	<div id="app">
-		<label for="teamSearch">Search teams </label>
-		<input type="text" name="teamSearch" v-model="searchTerm" v-on:keyup="search">
-		<ul>
-			<li v-for="team in searchResults" v-bind:key="team.id">
+		<div class="searchBar">
+			<label for="teamSearch">Search Teams</label>
+			<br />
+			<input
+				type="text"
+				name="teamSearch"
+				v-model="searchTerm"
+				v-on:keyup="search"
+				placeholder="ex. Toronto Maple Leafs"
+			/>
+		</div>
+
+		<ul class="searchResults">
+			<li
+				v-for="team in searchResults"
+				v-bind:key="team.id"
+				@click="openTeam(team)"
+			>
 				{{ team.name }}
 			</li>
 		</ul>
+
+		<team-box ref="teamBox"></team-box>
 	</div>
 </template>
 
 <script>
 import axios from "axios";
+import TeamBox from "./components/TeamBox";
 export default {
 	name: "App",
 	data() {
 		return {
 			teams: [],
 			searchTerm: "",
-			searchResults: []
+			searchResults: [],
 		};
 	},
-	components: {},
+	components: {
+		TeamBox,
+	},
 	methods: {
-		search: function(){
-			this.searchResults = this.teams.filter(t => t.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1)
-		}
+		search: function () {
+			this.searchResults = this.teams.filter(
+				(t) =>
+					t.name
+						.toLowerCase()
+						.indexOf(this.searchTerm.toLowerCase()) > -1
+			);
+		},
+		openTeam: function (team) {
+			this.$refs.teamBox.team = team;
+			this.$refs.teamBox.visible = true;
+		},
 	},
 	mounted() {
 		axios
@@ -43,7 +71,44 @@ export default {
 	margin: 0px;
 	padding: 0px;
 }
+
 :root {
-	--light: #ededed;
+	--highlight: #38ada9;
+	--light: #e9e9e9;
+	font-family: Arial, Helvetica, sans-serif;
+}
+
+.searchBar {
+	font-size: 25px;
+	text-align: center;
+	padding: 20px;
+}
+
+.searchResults {
+	list-style: none;
+	text-align: center;
+	width: 50%;
+	margin-left: 25%;
+}
+
+.searchResults li {
+	padding: 5px;
+	margin: 2px;
+	cursor: pointer;
+}
+
+.searchResults li:hover {
+	background: var(--light);
+}
+
+.searchBar input {
+	width: 50%;
+	font-size: 20px;
+	border: none;
+	background: var(--light);
+	text-align: center;
+	padding: 5px;
+	margin: 5px;
+	box-shadow: 0px 2px 0px var(--highlight);
 }
 </style>
---
+++ src/frontend/src/components/TeamBox.vue
@@ -0,0 +1,42 @@
+<template>
+	<div v-if="visible" class="container">
+		<div class="close" @click="visible = false">X</div>
+		<h1>
+			{{ team.name }}
+		</h1>
+		<h2>est. {{ team.firstYearOfPlay }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "TeamView",
+	data() {
+		return {
+			team: {},
+			visible: false,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.container {
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	box-sizing: border-box;
+	padding: 50px;
+	background: var(--light);
+	top: 0px;
+	left: 0px;
+}
+
+.close {
+	position: absolute;
+	right: 50px;
+	top: 50px;
+	font-size: 30px;
+	cursor: pointer;
+}
+</style>
