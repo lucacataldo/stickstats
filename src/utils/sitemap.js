928d66749386e9d6e0a2f402904468814efb94a9
Luca Cataldo <35984346+lucacataldo@users.noreply.github.com>
2020-12-31 23:09:21 -0500

    fix sitemap xml

--- src/utils/sitemap.js
+++ src/utils/sitemap.js
@@ -14,8 +14,7 @@ let generate = async () => {
 
   let teams = resp.data.teams
 
-  fs.writeFileSync("sitemap.xml", `
-<?xml version="1.0" encoding="UTF-8"?>\n
+  fs.writeFileSync("sitemap.xml", `<?xml version="1.0" encoding="UTF-8"?>\n
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n
   `)
   teams.forEach(team => {
