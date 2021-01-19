bd4bf40b1416a3585f2279676ce3b6ddb6338841
Luca Cataldo <35984346+lucacataldo@users.noreply.github.com>
2021-01-18 22:32:57 -0500

    Easter Seals link

--- src/frontend/src/views/Dangle.vue
+++ src/frontend/src/views/Dangle.vue
@@ -3,10 +3,26 @@
 		<h1>
 			WHY NOT?!
 		</h1>
-    <div>Check out stickstats.club for stats & more!</div>
-    <br>
     <div>
+      Check out 
+      <br>
+      <br>
+      <a class="button" href="https://stickstats.club">
+        stickstats.club
+      </a> 
+      <br>
+      <br>
+      for stats & more!</div>
+    <br>
+    <h3>
       Go Leafs Go &lt;3 
+    </h3>
+    <br>
+    <br>
+    <h2>And while you're at it,</h2>
+    <div>
+      <br>
+      <a target="_blank" rel="noopener" class="button" href="https://www.easterseals.org/">Donate to Easter Seals</a>
     </div>
 	</div>
 </template>
@@ -14,9 +30,7 @@
 <script>
 export default {
   mounted() {
-    setTimeout(() => {
-      window.location = "https://stickstats.club"
-    }, 5000);
+
   },
 }
 </script>
